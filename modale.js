be6bad4d97daa421e1db87f0f1fbdb512878d38e
Maïté N Mac <ninamaite1@gmail.com>
2021-05-05 23:34:38 +0200

    burger fix,  bug participation

--- modale.js
+++ modale.js
@@ -1,27 +1,31 @@
 function main(){
-  function editNav() {
-    let topNav = document.getElementById("myTopnav");
-    if (topNav.className === "topnav") {
-      x.className += " responsive";
-    } else {
-      x.className = "topnav";
-    }
-  }
 
   //////////////// DOM Elements //////////////
   const modalbg = document.querySelector(".bground");
   const modalBtn = document.querySelectorAll(".modal-btn");
-  const exitBtn = document.querySelectorAll(".close");
-  // const submitBtn = document.querySelectorAll(".btn-submit");
+  const exitBtn = document.querySelectorAll("#close");
+  const formData = document.querySelectorAll(".formData");
+  const burger = document.querySelector(".icon");
+  const form = document.getElementById('form')
+  const btnSubmit = document.querySelector(".btn-submit")
 
-  // const inputLine = document.querySelectorAll(".formData");  
-  // const errData = document.createElement('span');
+  // formData.forEach((div) => div.setAttribute('data-error',''));
+  // formData.forEach((div) => div.setAttribute('data-error-visible','false'));
 
-  // inputLine.forEach(inputLine.appendChild(errData))
-
-
+  console.log(formData)
 
 
+
+  /////////MENU RESPONSIVE////////
+  burger.onclick = function editNav() {
+    let topNav = document.getElementById("myTopnav");
+    if (topNav.className === "topnav") {
+      topNav.className += " responsive";
+    } else {
+      topNav.className = "topnav";
+    }
+  }
+  /////FIN MENU RESPONSIVE////////
 
 
 
@@ -29,7 +33,7 @@ function main(){
   //(OK)modal must contain click and "echap" conditions to exit the window
   //(OK)modal navigable /w tab key
   //(OK)must prevent enter key from closing the modal
-  //focus in checkbox, a voir
+  //(OK)focus in checkbox, a voir
 
   // launch modal event
   modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -54,9 +58,17 @@ function main(){
     }
   }
 
+    //prevent enter key from closing modal element and send the form
+    document.addEventListener('keydown', function(eventkey){
+      if (eventkey.key === 'Enter'){
+        eventkey.preventDefault();
+      }
+    })
+
+///////// FIN MODAL ELEMENT////////
 
 
-////////////////// FOCUS TRAP ///////////////////
+///////////// FOCUS TRAP //////////
 
   //modal elements for the focus trap
   const modalElts = document.querySelectorAll(".text-control, .checkbox-input, .checkbox-label, .checkbox2-label, .btn-submit")
@@ -79,17 +91,10 @@ function main(){
       }
     }
   })
-//////////////END FOCUS TRAP ///////////////////
+/////////////END FOCUS TRAP ////////////////
 
 
-  //prevent enter key from closing modal element and send the form
-  document.addEventListener('keydown', function(eventkey){
-    if (eventkey.key === 'Enter'){
-      eventkey.preventDefault();
-    }
-  })
-
-  ////////////////////CHECK USER INPUT///////////////////////
+  /////////////CHECK USER INPUT//////////////
 
   //(OK)has the user filled in all required fields?
   //(OK)has the user entered valid text without numbers
@@ -97,170 +102,123 @@ function main(){
   // check BDay regex
   //(OK)check end of the email input /w ok btn to exit the modal
 
-  // document.getElementById('inscription').addEventListener('submit', validate);
+  form.onsubmit = function validate(event){
 
+  const inputs = document.forms["reserve"];
 
-  document.forms["reserve"].addEventListener('submit',validate)
-
-  function validate(event){
-
-
-  //   //CHECK USER INPUT /W REGEX
+  //CHECK USER INPUT /W REGEX
     const REGEX_NAMES =/^[a-zîïéèêëì]+([-'\s][a-zîïéèêëì][a-zéèêëìîï]+)?$/i;
     const REGEX_MAIL =/^[a-z][a-z0-9-_]+@[a-z]+.[a-z]+$/;
   //   const REGEX_BDAY = /^[0-9]{2}[\/.-][0-9]{2}[\/.-][1-2][0-9]{3}$/;
 
 
-  //this = document.forms[reserve]
-    let inputs = this;
-    let erreurMsg;
+  // MSG ERROR
+    let msg;
 
-    if (REGEX_NAMES.test(inputs["first"].value) === false) {
-      inputs["first"].style.border = "blue 3px solid";
-      erreurMsg = "Veuillez renseigner correctement ce champ.";
-    }
+    let erreur = {};
+    erreur.empty = "Veuillez renseigner ce champ.";
+    erreur.incorrect = "Veuillez renseigner correctement le champ.";
+    erreur.ville = "Veuillez choisir une ville.";
+    erreur.radio = "Veuillez accepter les conditions générales.";
+
+
+    if (REGEX_NAMES.test(inputs["first"].value) === false){
+      inputs["first"].style.border = "blue 2px solid";
+      msg = erreur.incorrect;
+      formData[0].setAttribute('data-error',msg)
+      formData[0].setAttribute('data-error-visible','true')
+      }
+      else{
+        inputs["first"].style.border = "unset";
+        formData[0].removeAttribute('data-error');
+        formData[0].removeAttribute('data-error-visible');
+      }
+
     if (REGEX_NAMES.test(inputs["last"].value) === false) {
-      inputs["last"].style.border = "green 3px solid";
-      erreurMsg = "Veuillez renseigner correctement ce champ.";
+      inputs["last"].style.border = "red 2px solid";
+      msg = erreur.incorrect;
+      formData[1].setAttribute('data-error',msg)
+      formData[1].setAttribute('data-error-visible','true')
+    }
+    else{
+      inputs["last"].style.border = "unset";
+      formData[1].removeAttribute('data-error');
+      formData[1].removeAttribute('data-error-visible');
     }
 
     if (REGEX_MAIL.test(inputs["email"].value) === false){
-      inputs["email"].style.border = "pink 3px solid";
-      erreurMsg = "Veuillez renseigner correctement ce champ.";
-    }
+      inputs["email"].style.border = "red 2px solid";
+      msg = erreur.incorrect;
+      formData[2].setAttribute('data-error',msg)
+      formData[2].setAttribute('data-error-visible','true')
+      }
+      else{
+        inputs["email"].style.border = "unset";
+        formData[2].removeAttribute('data-error');
+        formData[2].removeAttribute('data-error-visible');
+      }
+  
+      if (inputs["checkbox1"].checked === false) {
+        msg = erreur.radio
+        formData[6].setAttribute('data-error',msg)
+        formData[6].setAttribute('data-error-visible','true')
+      }
+      else{
+        inputs["checkbox1"].style.border = "unset";
+        formData[6].removeAttribute('data-error');
+        formData[6].removeAttribute('data-error-visible');
+      }
 
-    if (inputs["quantity"].value > 0) {
+      if (inputs["quantity"].value > 0) {
         let radios = document.getElementsByName('location');
         let checkRadio = false;
         for (let a = 0; a < radios.length; a++){
-          if (checkRadio == false){
+          if (checkRadio === false){
             if (radios[a].checked === true) {
               checkRadio = true;
+              formData[5].removeAttribute('data-error');
+              formData[5].removeAttribute('data-error-visible');
               break;
             }
           }
         }
+        //!checkradio -> checkradio=false
         if (!checkRadio){
-          event.preventDefault();
-          erreurMsg = "Veuillez renseignez ville.";
-          alert('non radio');
+          msg = erreur.ville;
+          formData[5].setAttribute('data-error',msg)
+          formData[5].setAttribute('data-error-visible','true')
+  
+          // event.preventDefault();
         }
       }
 
-      if (inputs["checkbox1"].checked === false) {
-        inputs["first"].style.border = "yellow 3px solid";
-        erreurMsg = "Veuillez renseigner correctement ce champ.";
+      for (let i = 0; i < inputs.length; i++) {
+        if (!inputs[i].value) {
+          inputs[i].style.border = "red 2px solid";
+          msg = erreur.empty
+          formData[i].setAttribute('data-error',msg)
+          formData[i].setAttribute('data-error-visible','true')
+        }  
       }
+  
+      if (msg){
+        event.preventDefault();
+        return false;
+      } 
 
-    for (let i = 0; i < inputs.length; i++) {
-      if (!inputs[i].value) {
-        inputs[i].style.border = "red 2px solid";
-        erreurMsg = "Veuillez renseigner ce champ.";
-      }
-    }
+      else {
+      inputs[i].style.border = "unset";
+      formData.forEach((div) => div.removeAttribute('data-error'));
+      formData.forEach((div) => div.removeAttribute('data-error-visible'));
 
-    if (erreurMsg){
-      event.preventDefault();
-      return false;
-    } else {
       alert('FORMuLAIRE VALIDEE')
-    }
+      }
+
+
+    
   }
 
-  //   //////////////////END CHECK USER INPUT///////////////////////
-
-
-
-  // //   ////////////////////CHECK USER INPUT///////////////////////
-
-  // // //(OK)has the user filled in all required fields?
-  // // //(OK)has the user entered valid text without numbers
-  // // //(OK)is the birth date complete ?
-  // // // check BDay regex
-  // // //(OK)check end of the email input /w ok btn to exit the modal
-
-  // // document.getElementById('inscription').addEventListener('submit', validate);
-
-  // // function validate(event){
-  // //   //USER INPUT VALUES
-  // //   const firstName = document.getElementById('first');
-  // //   const lastName = document.getElementById('last');
-  // //   const userEmail = document.getElementById('email');
-  // //   const userBDay = document.getElementById('birthdate');
-  // //   const userParticipation = document.getElementById('quantity');
-  // //   const checkboxRequired = document.getElementById('checkbox1');
-
-  // //   //CHECK USER INPUT /W REGEX
-  // //   const REGEX_NAMES =/^[a-zîïéèêëì]+([-'\s][a-zîïéèêëì][a-zéèêëìîï]+)?$/i;
-  // //   const REGEX_MAIL =/^[a-z][a-z0-9-_]+@[a-z]+.[a-z]+$/;
-  // //   const REGEX_BDAY = /^[0-9]{2}[\/.-][0-9]{2}[\/.-][1-2][0-9]{3}$/;
-
-  // //   if ((firstName.value === "") || (lastName.value === "")){
-  // //     let erreur;
-
-  // //       event.preventDefault();
-  // //       erreur = "Veuillez renseignez ce champ";
-  // //       firstName.style.border = "red 2px solid";
-
-  // //     } else if ((REGEX_NAMES.test(firstName.value) === false) || (REGEX_NAMES.test(lastName.value) === false)){
-  // //       event.preventDefault();
-
-  // //   } else {
-  // //       checkEmail(event);
-  // //   }
-
-  // //   function checkEmail(event){
-  // //     let erreur;
-  // //     if (userEmail.value === "") {
-  // //       event.preventDefault();
-  // //       erreur = "Veuillez renseignez ce champ";
-
-  // //     } else if (REGEX_MAIL.test(userEmail.value) === false){
-  // //       event.preventDefault();
-  // //       alert("nop email");
-  // //     } else {
-  // //       checkBDay(event);
-  // //     }
-  // //   }
-
-  // //   // REVOIR REGEX BDAY APPAREMENT
-
-
-  // //   function checkBDay(event){
-  // //     if (userBDay.value === "") {
-  // //       event.preventDefault();
-  // //       alert("empty birthday")
-  // //     // } else if (REGEX_BDAY.test(userBDay) === false){
-  // //     //   alert("non regex birthday")
-  // //     } else {
-  // //       checkParticpation(event);
-  // //     }
-  // //   }
-
-  // //   function checkParticpation(event){
-  // //     if (userParticipation.value == "") {
-  // //       event.preventDefault();
-  // //       alert("inserer nombre participation meme zero")
-  // //     } else if (userParticipation.value < 0 || userParticipation.values > 10) {
-  // //       event.preventDefault();
-  // //       alert("impossible ! reessayer")
-  // //     } else {
-  // //       checkCheckboxes(event);
-  // //     }
-  // //   }
-
-  // //   function checkCheckboxes(event){
-  // //     if (checkboxRequired.checked === false) {
-  // //       event.preventDefault();
-  // //       alert("merci de cocher la case obligatoire");
-  // //     } else {
-  // //       alert('great');
-  // //     }
-  // //   }
-
-  // // }
-
-  // //   //////////////////END CHECK USER INPUT///////////////////////
+///////////END CHECK USER INPUT////////////
 
 }
 main();
